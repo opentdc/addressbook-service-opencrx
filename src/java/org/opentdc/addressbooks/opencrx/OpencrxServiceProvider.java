4e8be36f0dc57089018709610c665a3d8522953f
Bruno Kaiser <bruno@bkaiser.ch>
2015-08-21 23:10:57 +0200

    consistent ordering of query and queryType

--- src/java/org/opentdc/addressbooks/opencrx/OpencrxServiceProvider.java
+++ src/java/org/opentdc/addressbooks/opencrx/OpencrxServiceProvider.java
@@ -241,8 +241,8 @@ public class OpencrxServiceProvider extends AbstractOpencrxServiceProvider imple
 	 */
 	@Override
 	public List<AddressbookModel> list(
-		String queryType,
 		String query,
+		String queryType,
 		int position,
 		int size
 	) {
